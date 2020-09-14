3213c00f0df3f0833edd40563c6efa58b5832d5f
DrakeGoCoding <hoanglongphi21@gmail.com>
2020-09-14 21:20:27 +0700

    finish session3 theory

---
+++ session3_datatype/datatype.js
@@ -0,0 +1,30 @@
+// let a = 5;
+// let b = 2;
+// let c = a / b;
+
+// console.log(c);
+
+// let name = "Bob";  // or let name = 'Bob';
+// let greeting = `Hi ${name}`;
+// console.log(greeting);
+
+// let test1 = "Abcd" - "efgh";
+// let test2 = "abcd" - 1;
+// console.log(test1, test2);
+
+// let firstNum = Number(prompt("Enter first number"));
+// let secondNum = Number(prompt("Enter second number"));
+
+// let sum = firstNum + secondNum;
+// let diff = firstNum - secondNum;
+// let product = firstNum * secondNum;
+// let quotient = firstNum / secondNum;
+
+// console.log(`Sum: ${sum}`);
+// console.log(`Difference: ${diff}`);
+// console.log(`Product: ${product}`);
+// console.log(`Quotient: ${quotient}`);
+
+let a = 10;
+console.log(a++);
+console.log(a);
